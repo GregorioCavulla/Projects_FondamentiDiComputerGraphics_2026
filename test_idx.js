d838f8316e9bb6fbbba383f4298c0777b7110f5b
GregorioCavulla::ghigo <101806375+GregorioCavulla@users.noreply.github.com>
2026-07-13 14:21:09 +0200

    Clean root: remove test files and old documentation

--- test_idx.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('Modulo1/Progetto/project/porygon_data.js','utf8');
-code = code.replace(/const porygonSolids/g, 'global.porygonSolids');
-eval(code);
-console.log('Total indices array length:', porygonIndices.length);
-let sum = porygonParts.reduce((acc, p) => acc + p.indexCount, 0);
-console.log('Sum of parts indices:', sum);
